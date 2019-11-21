c8e80e476778fda147b4df0be2ec78d243fd0829
Bengt Ericsson <bengt.ericsson@polisen.se>
2019-11-21 14:41:06 +0100

    Initial commit

---
+++ ContentPresenterDemo/ItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentPresenterDemo
+{
+    public class ItemViewModel
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ ContentPresenterDemo/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace ContentPresenterDemo
+{
+    using System.Collections.Generic;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new List<ItemViewModel>
+                          {
+                              new ItemViewModel { Value = 1 }, 
+                              new ItemViewModel { Value = 2 }, 
+                              new ItemViewModel { Value = 3 }
+                          };
+        }
+    }
+}
